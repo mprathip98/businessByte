0b6d2c514612b281a4ca2c2660f12d34dbd17be8
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-10-19 20:43:54 -0700

    rendering

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello, world. You're at the polls page.")
+    return render(request, 'hello.html')

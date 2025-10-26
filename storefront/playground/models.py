02b4437ec32f3457932c13f35b40c18f8c3bd4eb
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-10-25 19:11:12 -0700

    model update #1

--- storefront/playground/models.py
+++ storefront/playground/models.py
@@ -6,3 +6,6 @@ class Members(models.Model):
     email = models.EmailField(max_length=100)
     passwd = models.CharField(max_length=50)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.fname + " " + self.lname

a55a9eac2c41bc7aaf2bf39f742fa4e47b5e1c74
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-10-25 19:00:28 -0700

    first model

--- storefront/playground/models.py
+++ storefront/playground/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Members(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField()

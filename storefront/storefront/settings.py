73422242a8b4de296fd081f7f66c1f60923cadad
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-11-09 10:40:24 -0700

    initial user auth

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sessions',
-    'playground'
+    'playground',
+    'members'
 ]
 
 MIDDLEWARE = [

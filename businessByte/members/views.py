e416f25bd05b7453e30d0cd07deae0b438e595bb
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-11-26 10:44:14 -0700

    basic views added

--- businessByte/members/views.py
+++ businessByte/members/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def login_user(request):
+    return render(request, "authenticate/login.html", {})
+
+def logout_user(request):
+    pass
+
+def register_user(request):
+    return render(request, "authenticate/register.html", {})

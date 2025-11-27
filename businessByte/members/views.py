276ab5d84df5c6c66a6d46cc17c8487fd74a6038
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-11-27 16:15:02 -0700

    functional login page

--- businessByte/members/views.py
+++ businessByte/members/views.py
@@ -4,7 +4,18 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
 
 def logout_user(request):
     pass

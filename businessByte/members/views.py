56ec0d308a55212a142e97944e1cfbbcd0961f24
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-11-28 11:51:52 -0700

    successful registration + auth

--- businessByte/members/views.py
+++ businessByte/members/views.py
@@ -30,12 +30,16 @@ def register_user(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            first_name = form.cleaned_data['name']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, first_name=first_name, username=username, password=password)
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')
+        else:
+            messages.error(request, "")
+            return redirect('register')
 
     else:
         form = UserRegisterForm()

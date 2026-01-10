46e23918a2b8a2aa6420cca59e980bce7cd8b76c
Mohit Prathipati <prathipati.mohit@gmail.com>
2026-01-10 11:52:45 -0700

    deals page initial

--- businessByte/members/forms.py
+++ businessByte/members/forms.py
@@ -9,9 +9,3 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
-
-#
-# class favoritesForm(forms.ModelForm):
-#     class Meta:
-#         model = favorites
-#         fields = ["businessName", "nameUser"]
